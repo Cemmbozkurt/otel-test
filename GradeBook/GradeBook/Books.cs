298907e7c08ef1e13a8ee7c1ec90a79c308871f7
Cem Bozkurt <cem.bozkurt@siemens.com>
2022-04-28 13:32:23 +0300

    git commit message example

---
+++ GradeBook/GradeBook/Books.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class Books
+    {
+        public Books(String name)
+        {
+            grades = new List<double>();  
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public List<double> grades;
+        public string name;
+        private double average = 0;
+        private double lowest = 120, highest, number;
+
+        public void ShowStatistic()
+        {
+            for (int i = 0; i < this.grades.Count; ++i)
+            {
+                Console.WriteLine(this.grades[i]);
+            }
+
+            Console.WriteLine("Calculating....\n");
+
+            for (int i = 0; i < this.grades.Count; ++i)
+            {
+                highest = Math.Max(this.grades[i], highest);
+                lowest = Math.Min(this.grades[i], lowest);
+                average += this.grades[i];
+            }
+
+            average = average / this.grades.Count;
+            Console.WriteLine("Lowest" + lowest);
+            Console.WriteLine("Highest" + highest);
+            Console.WriteLine("Average" + average);
+
+        }
+
+    }
+}
---
+++ GradeBook/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Books book1 = new Books("Class Records!");
+            book1.AddGrade(90.2);
+            book1.AddGrade(100);
+            book1.AddGrade(90);
+            book1.AddGrade(100);
+            book1.ShowStatistic();
+
+            var input = Console.ReadLine();
+ 
+
+
+        }
+    }
+}
